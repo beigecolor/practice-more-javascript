93fd2f1bf7f939bc10bc2502d46c558b088db02d
Jerusalem Lipardo <beigecolor29@gmail.com>
2019-09-16 13:32:34 -0700

    pushed p0

--- P0/problems/1_average.js
+++ P0/problems/1_average.js
@@ -1,15 +1,16 @@
 /******************************************************************************
-** Write a function average(num1, num2) that returns the average of two numbers.
-**
-** Examples:
-**
-** average(10, 20); // => 15
-** average(5, 7); // => 6
-** average(24, 32); // => 28
-*/
+ ** Write a function average(num1, num2) that returns the average of two numbers.
+ **
+ ** Examples:
+ **
+ ** average(10, 20); // => 15
+ ** average(5, 7); // => 6
+ ** average(24, 32); // => 28
+ */
 
 function average(num1, num2) {
-  // your code here...
+  let avg = (num1 + num2) / 2;
+  return avg;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
--- P0/problems/2_tripler.js
+++ P0/problems/2_tripler.js
@@ -1,17 +1,21 @@
 /*******************************************************************************
-** Write a function tripler(array) that takes in an array and returns a new array
-** containing 3 times every element of the original array.
-**
-** Examples:
-**
-** tripler([1,2,3]); // => [ 3, 6, 9 ]
-** tripler([4, 1, 7]); // => [ 12, 3, 21 ]
-*/
+ ** Write a function tripler(array) that takes in an array and returns a new array
+ ** containing 3 times every element of the original array.
+ **
+ ** Examples:
+ **
+ ** tripler([1,2,3]); // => [ 3, 6, 9 ]
+ ** tripler([4, 1, 7]); // => [ 12, 3, 21 ]
+ */
 
 function tripler(array) {
-  // your code here...
+  let triple = [];
+  for (var i = 0; i < array.length; i += 1) {
+    let newArr = array[i] * 3;
+    triple.push(newArr);
+  }
+  return triple;
 }
-
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 module.exports = tripler;
