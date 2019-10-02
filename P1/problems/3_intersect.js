72a385bef586fd074984d7dcd404f8a306ab6f36
Jerusalem Lipardo <beigecolor29@gmail.com>
2019-10-01 23:24:16 -0700

    intesect

--- P1/problems/3_intersect.js
+++ P1/problems/3_intersect.js
@@ -11,7 +11,14 @@ intersect(['a', 'b', 'c'], ['x', 'y', 'z']) => []
 *******************************************************************************/
 
 function intersect(arr1, arr2) {
-  // your code here...
+  let midpoint = [];
+  for (var i = 0; i < arr1.length; i += 1) {
+    for (var j = 0; j < arr2.length; j += 1) {
+      if (arr1[i] === arr2[i]) {
+        midpoint.push();
+      }
+    }
+  }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
