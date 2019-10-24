742e0ae30afc382f709dd902649937e7922112db
Jerusalem Lipardo <beigecolor29@gmail.com>
2019-10-23 17:06:12 -0700

    mirror array

--- w2d1_pset/problems/problems/2_mirror_array.js
+++ w2d1_pset/problems/problems/2_mirror_array.js
@@ -9,7 +9,13 @@ mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a
 ***********************************************************************/
 
 function mirrorArray(array) {
+  let newArr = array.slice(0, array.length);
 
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    let rev = array[i];
+    newArr.push(rev);
+  }
+  return newArr;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
