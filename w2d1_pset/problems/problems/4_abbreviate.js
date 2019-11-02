b7aced4aa43f20a1a5ceb631ffba11c2414cecbd
Jerusalem Lipardo <beigecolor29@gmail.com>
2019-11-01 21:44:15 -0700

    abbreviate

--- w2d1_pset/problems/problems/4_abbreviate.js
+++ w2d1_pset/problems/problems/4_abbreviate.js
@@ -13,11 +13,34 @@ abbreviate('programming is fantastic'); // => 'prgrmmng is fntstc'
 abbreviate('hello world'); // => 'hll wrld'
 abbreviate('how are you'); // => 'how are you'
 ***********************************************************************/
-function removeVowels(words) {
+function removeVowels(word) {
   let vowels = ["a", "e", "i", "o", "u"];
   let newWord = "";
+
+  for (let i = 0; i < word.length; i += 1) {
+    let char = word[i];
+    if (vowels.indexOf(char) === -1) {
+      newWord += char;
+    }
+  }
+  return newWord;
 }
-function abbreviate(sentence) {}
+function abbreviate(sentence) {
+  let words = sentence.split(" ");
+  let newWords = [];
+
+  for (let i = 0; i < words.length; i += 1) {
+    let word = words[i];
+
+    if (word.length >= 5) {
+      newWords.push(removeVowels(word));
+    } else {
+      newWords.push(word);
+    }
+  }
+  let newSent = newWords.join(" ");
+  return newSent;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = abbreviate;
