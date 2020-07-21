baabf7c9776c46c1ed09b75940c98d56ba03faa2
Jerusalem Lipardo <beigecolor29@gmail.com>
2020-07-20 19:23:21 -0700

    next two primes

--- w2d4_pset/problem_set/problems/7_next_two_primes.js
+++ w2d4_pset/problem_set/problems/7_next_two_primes.js
@@ -10,9 +10,28 @@ nextTwoPrimes(8); // => [ 11, 13 ]
 nextTwoPrimes(20); // => [ 23, 29 ]
 nextTwoPrimes(97); // => [ 101, 103 ]
 ***********************************************************************/
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function nextTwoPrimes(num) {
+  let nums = [];
 
+  for (let i = num + 1; nums.length < 2; i += 1) {
+    if (isPrime(i)) {
+      nums.push(i);
+    }
+  }
+  return nums;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
