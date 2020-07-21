5604986a49b750c46fa993cf532df50ba922fea4
Jerusalem Lipardo <beigecolor29@gmail.com>
2020-07-20 18:21:33 -0700

    cap vowels

--- w2d4_pset/problem_set/problems/6_cap_vowels.js
+++ w2d4_pset/problem_set/problems/6_cap_vowels.js
@@ -13,7 +13,19 @@ capVowels('boOtCamP PreP'); // => 'bOOtcAmp prEp'
 ***********************************************************************/
 
 function capVowels(string) {
+  let vowels = "aeiouAEIOU".split("");
+  let newWords = "";
 
+  for (let i = 0; i < string.length; i += 1) {
+    let chars = string[i];
+
+    if (vowels.indexOf(chars) > -1) {
+      newWords += chars.toUpperCase();
+    } else {
+      newWords += chars.toLowerCase();
+    }
+  }
+  return newWords;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
