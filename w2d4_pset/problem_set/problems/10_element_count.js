76aad0f7e278552b99578f7756e94f273b156a9f
Jerusalem Lipardo <beigecolor29@gmail.com>
2020-07-23 09:49:27 -0700

    element count

--- w2d4_pset/problem_set/problems/10_element_count.js
+++ w2d4_pset/problem_set/problems/10_element_count.js
@@ -9,7 +9,18 @@ elementCount(['c', 'a', 'c', 'a', 'b']); // => { "c": 2, "a": 2, "b": 1 }
 ***********************************************************************/
 
 function elementCount(array) {
+  let objCount = {};
 
+  for (let i = 0; i < array.length; i += 1) {
+    let key = array[i];
+
+    if (objCount[key] === undefined) {
+      objCount[key] = 1;
+    } else {
+      objCount[key] += 1;
+    }
+  }
+  return objCount;
 }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = elementCount;
