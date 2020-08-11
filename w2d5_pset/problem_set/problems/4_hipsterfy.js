4ed2132fde45edf67ed101db5d318d21d199d0b2
Jerusalem Lipardo <beigecolor29@gmail.com>
2020-08-11 16:59:50 -0700

    hipsterly

--- w2d5_pset/problem_set/problems/4_hipsterfy.js
+++ w2d5_pset/problem_set/problems/4_hipsterfy.js
@@ -11,9 +11,28 @@ hipsterfy("towel flicker banana"); // => "towl flickr banan"
 hipsterfy("runner anaconda"); // => "runnr anacond"
 hipsterfy("turtle cheeseburger fries"); // => "turtl cheeseburgr fris"
 ***********************************************************************/
+function hipsterflyWord(word) {
+  let vowel = ["a", "e", "i", "o", "u"];
+
+  for (let i = word.length - 1; i >= 0; i -= 1) {
+    if (vowel.indexOf(word[i]) !== -1) {
+      let hipsterWord = word.slice(0, i) + word.slice(i + 1);
+      return hipsterWord;
+    }
+  }
+  return word;
+}
 
 function hipsterfy(sentence) {
+  let hipsterfiedWords = [];
+  let words = sentence.split(" ");
 
+  for (let i = 0; i < words.length; i += 1) {
+    let word = words[i];
+    hipsterfiedWords.push(hipsterflyWord(word));
+  }
+  let hipsterfiedSentence = hipsterfiedWords.join(" ");
+  return hipsterfiedSentence;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
