964cc0eb8de2a5ad3cac1e3088a11af3ea2767a4
Jerusalem Lipardo <beigecolor29@gmail.com>
2020-08-14 10:45:16 -0700

    solving three in a row

--- w3d1_pset/problem_set/problems/2_three_in_a_row.js
+++ w3d1_pset/problem_set/problems/2_three_in_a_row.js
@@ -10,7 +10,11 @@ threeInARow([10, 9, 20, 33, 3, 3]); // => false;
 ***********************************************************************/
 
 function threeInARow(arr) {
+  let num1 = 0;
+  let num2 = 0;
+  let num3 = 0;
 
+  for (let i = 0; i < arr.length; i += 1) {}
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
