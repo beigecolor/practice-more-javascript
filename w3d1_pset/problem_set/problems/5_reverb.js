6f1071a5df684815effcd20bb2e7415a76052b2c
Jerusalem Lipardo <beigecolor29@gmail.com>
2020-08-24 08:57:17 -0700

    reverb

--- w3d1_pset/problem_set/problems/5_reverb.js
+++ w3d1_pset/problem_set/problems/5_reverb.js
@@ -11,7 +11,17 @@ reverb('my'); // => 'my'
 ***********************************************************************/
 
 function reverb(word) {
+  let vowels = ["a", "e", "i", "o", "u"];
 
+  for (let i = word.length - 1; i >= 0; i -= 1) {
+    let char = word[i];
+
+    if (vowels.indexOf(char) > -1) {
+      let after = word.slice(i);
+      return word + after;
+    }
+  }
+  return word;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
