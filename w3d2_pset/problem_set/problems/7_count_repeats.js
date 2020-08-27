1b7bb8cad42bc2c3c55aeb6d19f39f6649ce8e79
Jerusalem Lipardo <beigecolor29@gmail.com>
2020-08-27 16:43:24 -0700

    count repeat

--- w3d2_pset/problem_set/problems/7_count_repeats.js
+++ w3d2_pset/problem_set/problems/7_count_repeats.js
@@ -13,9 +13,31 @@ countRepeats('pops'); //=> 1
 countRepeats('mississippi'); //=> 3
 countRepeats('hellobootcampprep'); //=> 4
 ***********************************************************************/
+function charCount(str) {
+  let count = {};
+
+  for (let i = 0; i < str.length; i += 1) {
+    let char = str[i];
+
+    if (count[char] === undefined) {
+      count[char] = 1;
+    } else {
+      count[char] += 1;
+    }
+  }
+  return count;
+}
 
 function countRepeats(string) {
+  let count = charCount(string);
+  let repeat = 0;
 
+  for (var char in count) {
+    if (count[char] > 1) {
+      repeat += 1;
+    }
+  }
+  return repeat;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
