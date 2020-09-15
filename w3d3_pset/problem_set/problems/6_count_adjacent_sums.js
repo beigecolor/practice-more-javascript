b6d420a97ee7e81904b3e8c9cded1aa999e19bca
Jerusalem Lipardo <beigecolor29@gmail.com>
2020-09-15 09:31:30 -0700

    adjacent

--- w3d3_pset/problem_set/problems/6_count_adjacent_sums.js
+++ w3d3_pset/problem_set/problems/6_count_adjacent_sums.js
@@ -11,7 +11,14 @@ countAdjacentSums([6, 7, 11, 2, 5, 10, 3], 13) //=> 3
 ***********************************************************************/
 
 function countAdjacentSums(arr, n) {
+  let count = 0;
 
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] + arr[i + 1] === n) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
